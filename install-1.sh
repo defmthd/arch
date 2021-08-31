38d80d23a5be823baeefb9ed223bebfde9890498
Alexander Lomakin <alexander.lomakin@gmail.com>
2021-08-31 21:57:21 +0300

    Add initial files

---
+++ install-1.sh
@@ -0,0 +1,24 @@
+iwctl --passphrase [password] station wlan0 connect "SSID"
+
+timedatectl set-ntp true
+
+pacman -Syyy
+pacman -S reflector
+reflector -c Russia -a 6 --sort rate --save /etc/pacman.d/mirrorlist
+pacman -Syyy
+
+mkfs.vfat /dev/nvme0n1p1
+mkfs.ext4 /dev/nvme0n1p5
+
+mkdir /mnt/boot
+mkdir /mnt/windows11
+
+mount /dev/nvme0n1p1 /mnt/boot
+mount /dev/nvme0n1p3 /mnt/windows11
+mount /dev/nvme0n1p5 /mnt
+
+pacstrap /mnt base linux linux-firmware vim amd-ucode
+
+genfstab -U /mnt >> /mnt/etc/fstab
+
+arch-chroot /mnt
---
+++ install-2.sh
@@ -0,0 +1,35 @@
+fallocate -l 2GB /swapfile
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+
+echo /etc/fstab >> "/swapfile none swap defaults 0 0"
+
+ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
+hwclock --systohc
+timedatectl set-ntp true
+
+sed -d '177s/.//' /etc/locale.gen
+sed -d '403s/.//' /etc/locale.gen
+locale-gen
+
+echo "LANG=en_US.UTF-8" >> /etc/locale.con
+echo "Arch" >> /etc/hostname
+
+echo root:[password] | chpasswd
+
+pacman -S grub efibootmgr os-prober ntfs-3g networkmanager network-manager-applet wireless_tools wpa_supplicant dialog mtools dosfstools base-devel linux-headers git bluez bluez-utils pulseaudio-bluetooth xf86-video-amdgpu xorg 
+
+echo "GRUB_DISABLE_OS_PROBER=false" >> /etc/default/grub
+
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+
+grub-mkconfig -o /boot/grub/grub.cfg
+
+systemctl enable NetworkManager
+systemctl enable bluetooth
+
+useradd -mG wheel defmthd
+passwd defmthd
+
+# don't forget visudo ;)
