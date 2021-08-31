3244ce48bc13303ae998d7481e7823510eba7676
Alexander Lomakin <alexander.lomakin@gmail.com>
2021-08-31 23:25:08 +0300

    Fix

--- install-0.sh
+++ install-0.sh
@@ -3,3 +3,8 @@ timedatectl set-ntp true
 pacman -Syyy
 reflector -c Russia -a 6 --sort rate --save /etc/pacman.d/mirrorlist
 pacman -Syyy
+
+pacman -S git
+git clone https://github.com/defmthd/arch.git
+
+cd arch
