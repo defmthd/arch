3b6d219b592a30dee51ec6bc5975e2420ef507ec
Alexander Lomakin <alexander.lomakin@gmail.com>
2021-09-02 00:35:55 +0300

    Fix

--- install-3.sh
+++ install-3.sh
@@ -11,3 +11,6 @@ cd ..
 
 yay -Syu
 yay -S google-chrome github-desktop vscode slack-desktop polybar ttf-windows ttf-font-awesome nerd-fonts-iosevka noto-fonts-emoji
+
+gsettings set org.gnome.desktop.wm.preferences titlebar-font 'Iosevka 10'
+gsettings set org.gnome.desktop.interface monospace-font-name 'Iosevka 10'
