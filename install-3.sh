ad7262690a19863467b80ff15f6efd0e093f5f49
Alexander Lomakin <alexander.lomakin@gmail.com>
2021-09-02 00:31:07 +0300

    Fix

--- install-3.sh
+++ install-3.sh
@@ -1,4 +1,4 @@
-sudo pacman -S xorg xorg-xinit i3-gaps i3status dmenu terminator alacritty
+sudo pacman -S xorg xorg-xinit i3-gaps i3status dmenu terminator alacritty stow feh thunar lxappearance picom
 
 sudo cp /etc/X11/xinit/xinitrc ~/.xinitrc
 
@@ -9,7 +9,5 @@ cd yay
 makepkg -si
 cd ..
 
-# yay -Syu
-# yay -S google-chrome github-desktop
-
-# sudo pacman -S feh thunar lxappearance picom nerd-font-iosevka fonts-font-awesome
+yay -Syu
+yay -S google-chrome github-desktop vscode slack-desktop polybar ttf-windows ttf-font-awesome nerd-fonts-iosevka noto-fonts-emoji
