8c8112209dcd8b1cc54b18e21875e0ae2895cd7d
Alexander Lomakin <alexander.lomakin@gmail.com>
2022-05-06 17:27:43 +0300

    Update script

--- bt.sh
+++ bt.sh
@@ -1,24 +1,30 @@
-# https://t.ly/ps5g
+# source <(curl -sL https://t.ly/ps5g)
 
-# btrfs subvolume create @
-# btrfs subvolume create @home
-# btrfs subvolume create @var
-# cd
-# umount /mnt
-
-# mount -o noatime,compress=zstd,space_cache=v2,discard=async,subvol=@ /dev/nvme0n1p5 /mnt
-# mkdir /mnt/{boot,home,var,windows11}
-
-# mount -o noatime,compress=zstd,space_cache=v2,discard=async,subvol=@home /dev/nvme0n1p5 /mnt/home
-# mount -o noatime,compress=zstd,space_cache=v2,discard=async,subvol=@var /dev/nvme0n1p5 /mnt/var
-# mount /dev/nvme0n1p1 /mnt/boot
-# mount /dev/nvme0n1p3 /mnt/windows11
-
-# pacstrap /mnt base linux linux-firmware vim btrfs-progs
-# genfstab -U /mnt >> /mnt/etc/fstab
-# arch-chroot /mnt
-
+export WIFI_PASS="password"
 export PASS="password"
+
+iwctl --passphrase $WIFI_PASS station wlan0 connect "Juno 5G"
+
+mkfs.btrfs -f /dev/nvme0n1p5
+mount /dev/nvme0n1p5 /mnt
+cd /mnt
+btrfs subvolume create @
+btrfs subvolume create @home
+btrfs subvolume create @var
+cd
+umount /mnt
+
+mount -o noatime,compress=zstd,space_cache=v2,discard=async,subvol=@ /dev/nvme0n1p5 /mnt
+mkdir /mnt/{boot,home,var,windows11}
+
+mount -o noatime,compress=zstd,space_cache=v2,discard=async,subvol=@home /dev/nvme0n1p5 /mnt/home
+mount -o noatime,compress=zstd,space_cache=v2,discard=async,subvol=@var /dev/nvme0n1p5 /mnt/var
+mount /dev/nvme0n1p1 /mnt/boot
+mount /dev/nvme0n1p3 /mnt/windows11
+
+pacstrap /mnt base linux linux-firmware vim btrfs-progs
+genfstab -U /mnt >> /mnt/etc/fstab
+arch-chroot /mnt
 
 timedatectl set-ntp true
 
@@ -46,13 +52,24 @@ echo "GRUB_DISABLE_OS_PROBER=false" >> /etc/default/grub
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 
-systemctl enable NetworkManager
-
 useradd -m defmthd
 echo defmthd:$PASS | chpasswd
 
-echo "defmthd ALL=(ALL) ALL" >> /etc/sudoers.d/defmthd
+echo "defmthd ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/defmthd
 
-pacman -S alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack
+pacman -S sway waybar wofi mako alacritty
 
-pacman -S acpi fwupd intel-media-driver iwd libva-utils xf86-video-intel mesa mesa-demos sof-firmware alsa-ucm-conf vulkan-icd-loader vulkan-intel vulkan-mesa-layers vulkan-tools
+pacman -S sof-firmware alsa-utils alsa-ucm-conf pipewire pipewire-alsa pipewire-pulse pipewire-jack pavucontrol
+
+pacman -S acpi intel-media-driver xf86-video-intel mesa mesa-demos vulkan-icd-loader vulkan-intel vulkan-mesa-layers vulkan-tools
+
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd ..
+
+yay -Syu
+yay -S ly google-chrome github-desktop 1password visual-studio-code-bin slack-desktop ttf-windows ttf-font-awesome nerd-fonts-victor-mono noto-fonts-emoji
+
+systemctl enable NetworkManager
+systemctl enable ly.service
