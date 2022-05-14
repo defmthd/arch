b6a1868fc40eb9881d5900e59f3ac0ab53bc3837
Alexander Lomakin <alexander.lomakin@gmail.com>
2022-05-14 04:09:51 +0300

    Add more

--- b.sh
+++ b.sh
@@ -20,7 +20,7 @@ pacman -S rsync reflector
 reflector -c Russia -a 6 --sort rate --save /etc/pacman.d/mirrorlist
 pacman -Syyy
 
-pacman -S git grub grub-btrfs efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools ntfs-3g base-devel linux-headers os-prober
+pacman -S htop git grub grub-btrfs efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools ntfs-3g base-devel linux-headers os-prober
 
 echo "GRUB_DISABLE_OS_PROBER=false" >> /etc/default/grub
 
@@ -32,7 +32,9 @@ echo defmthd:$PASS | chpasswd
 
 echo "defmthd ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/defmthd
 
-pacman -S sway waybar wofi mako alacritty
+# pacman -S sway waybar wofi mako alacritty
+
+pacman -S xorg xorg-xinit openbox nitrogen mmaker obconf lxappearance-obconf
 
 pacman -S sof-firmware alsa-utils alsa-ucm-conf pipewire pipewire-alsa pipewire-pulse pipewire-jack pavucontrol brightnessctl
 
@@ -50,7 +52,7 @@ makepkg -si
 cd ..
 
 yay -Syu
-yay -S ly google-chrome github-desktop 1password visual-studio-code-bin slack-desktop ttf-windows ttf-font-awesome nerd-fonts-victor-mono noto-fonts-emoji
+yay -S ly google-chrome github-desktop 1password visual-studio-code-bin slack-desktop ttf-windows ttf-font-awesome nerd-fonts-victor-mono nerd-fonts-fantasque-sans-mono noto-fonts-emoji
 
 systemctl enable NetworkManager
 systemctl enable ly.service
